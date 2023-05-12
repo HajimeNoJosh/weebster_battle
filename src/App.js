7c89844f4d86b828deefed1af5517b59a614b743
Josh Martin <josh.martin@visiblebody.com>
2023-05-11 23:17:40 -0400

    add scroll to top

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import { characters } from './components/characters';
 import GuessWhoBoard from './components/guess_who_board';
-import "./sass/main.scss";
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [showButton, setShowButton] = useState(false)
+  useEffect(() => {
+    const handleScrollButtonVisibility = () => {
+      window.pageYOffset > 300 ? setShowButton(true) : setShowButton(false);
+    }
+    window.addEventListener('scroll', handleScrollButtonVisibility)
+
+    return () => {
+      window.removeEventListener('scroll', handleScrollButtonVisibility)
+    }
+  }, [])
+
+  const handleScrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+  }
 
   return (
     <div className="app">
       <GuessWhoBoard characters={characters} />
+      {showButton && (
+        <div className='scrollToTop'>
+          <button className='p-4' onClick={handleScrollToTop}
+          >
+            <img src="up-arrow.svg" alt="scollToTop" />
+          </button>
+        </div>
+      )}
     </div>
   );
 }
