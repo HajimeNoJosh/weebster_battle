de4e1b23892acf74874fa8333f263c95277f8ac1
Josh Martin <josh.martin@visiblebody.com>
2023-05-13 15:09:46 -0400

    clean up code a bit

--- src/components/guess_who_board.js
+++ src/components/guess_who_board.js
@@ -6,13 +6,10 @@ function GuessWhoBoard({ characters }) {
     const [confirmDone, setConfirmDone] = useState(false);
 
     const handleClick = (character) => {
-
-        console.log(character.flipped)
-
         if (confirmDone === false) {
             setSelectedCharacter(character);
         } else {
-            const characterName = document.getElementsByClassName("character-" + character.name.full);
+            const characterName = document.getElementsByClassName("character-" + character.id);
             if (character.flipped === false || character.flipped === undefined) {
                 character.flipped = true
                 characterName[0].classList.add("character-flipped");
@@ -20,7 +17,6 @@ function GuessWhoBoard({ characters }) {
                 character.flipped = false
                 characterName[0].classList.remove("character-flipped");
             }
-
         }
     };
 
@@ -43,8 +39,8 @@ function GuessWhoBoard({ characters }) {
             <div className="board">
                 {characters.map((character) => (
                     <div key={character.id} className="row">
-                        <div className={"character character-" + character.name.full} onClick={() => handleClick(character)}>
-                            <img className={character.name.full} src={character.image.large} alt={character.name.full} />
+                        <div className={"character character-" + character.id} onClick={() => handleClick(character)}>
+                            <img className={character.id} src={character.image.large} alt={character.name.full} />
                             <p>{character.name.full}</p>
                         </div>
                     </div>
@@ -54,7 +50,5 @@ function GuessWhoBoard({ characters }) {
         </div >
     );
 }
-
-
 
 export default GuessWhoBoard;
