ee57ea3833334b2ace77a3b779656bb3e39c26ca
Josh Martin <josh.martin@visiblebody.com>
2023-05-14 21:33:41 -0400

    add cursor

--- src/components/board.js
+++ src/components/board.js
@@ -1,22 +1,18 @@
 import { Fragment } from 'react';
 
 function Board({ characters, handleClick }) {
-    const rows = [];
-    for (let i = 0; i < 24; i++) {
-        rows.push(i);
-    }
     return (
         <Fragment>
             <div className="board">
                 {characters.length > 0 ? characters.map((character) => (
                     <div key={character.id} className="row">
-                        <div className={"character character-" + character.id} onClick={() => handleClick(character)}>
+                        <div className={"character character-information character-" + character.id} onClick={() => handleClick(character)}>
                             <img className={character.id} src={character.image.large} alt={character.name.full} />
                             <p>{character.name.full}</p>
                         </div>
                     </div>
                 )) :
-                    rows.map((id) => (
+                    [...Array(24).keys()].map((id) => (
                         <div key={id} className="row">
                             <div className={"character"}>
                                 <img src="gradient.gif" alt="filler" />
